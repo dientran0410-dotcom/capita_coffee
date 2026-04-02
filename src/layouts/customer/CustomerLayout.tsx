0ef40fdea4ffa129ca23aeb8019c5454704b8920
Điền Trần <dientran0410@gmail.com>
2026-04-02 08:56:00 +0700

    deploy code

--- src/layouts/customer/CustomerLayout.tsx
+++ src/layouts/customer/CustomerLayout.tsx
@@ -1,4 +1,4 @@
-import SideBar from "../../components/layout/SideBar";
+import SideBar from "../../components/layout/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const CustomerLayout = () => {
